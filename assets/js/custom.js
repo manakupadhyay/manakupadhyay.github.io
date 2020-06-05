9632a19a9f076ccc83a319294d1562e2b60db793
Manak Upadhyay <manu742000@gmail.com>
2020-06-05 17:12:28 +0530

    Removed About Section, NavLinks

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,11 @@
-// function myFunction() {
-//   var element = document.body;
-//   element.classList.toggle("dark-mode");
-// }
+function copyText() {
+    var range = document.createRange();
+    range.selectNode(document.getElementById("connect-info"));
+    window.getSelection().removeAllRanges(); // clear current selection
+    window.getSelection().addRange(range); // to select text
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();// to deselect
+    let x = document.getElementById("connect-info").title
+    x = 'Copied'
+}
+
